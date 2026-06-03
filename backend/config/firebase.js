ae68c6e48274ea5051d7512c5ac6653c5e935851
abhigith23 <singhabhishek20695@gmail.com>
2026-06-03 19:49:23 +0530

    fix: firebase config for production

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -3,9 +3,9 @@ const admin = require('firebase-admin');
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId:    process.env.FIREBASE_PROJECT_ID,
-      privateKey:   process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      clientEmail:  process.env.FIREBASE_CLIENT_EMAIL,
+      projectId:   process.env.FIREBASE_PROJECT_ID,
+      privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`
   });
